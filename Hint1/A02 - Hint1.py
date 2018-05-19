e722b5a8728925cc411720d7bea4bd2d5a0372d6
BrianCoveney <coveneybrian@gmail.com>
2018-05-19 15:43:23 +0100

    Make cosmetic changes

--- Hint1/A02 - Hint1.py
+++ Hint1/A02 - Hint1.py
@@ -1,4 +1,3 @@
-1
 # --------------------------------------------------------
 #           PYTHON PROGRAM
 # Here is where we are going to define our set of...
@@ -10,7 +9,6 @@
 # the Python interpreter will load our program,
 # but it will execute nothing yet.
 # --------------------------------------------------------
-
 import json
 
 
@@ -30,32 +28,25 @@ def get_avg(val1, val2):
 
 
 # ------------------------------------------
-# FUNCTION process_items
+# FUNCTION process_items(x)
 # ------------------------------------------
 def process_items(x):
-    cuisine = x["cuisine"]
-    evaluation = x["evaluation"]
-    points = x["points"]
+    cuisine, evaluation, points = x["cuisine"], x["evaluation"], x["points"]
     return cuisine, (points, evaluation)
 
 
 # ------------------------------------------
-# FUNCTION aggregate_info
-# Aggregate info per type of cuisine
-# Example: (u'Donuts', (16, 0, 137))
+# FUNCTION aggregate_info(x)
+# Point 1. Aggregate info per type of cuisine
+#          Example: (u'Donuts', (16, 0, 137))
 # ------------------------------------------
 def aggregate_info(x):
-    cuisine = x[0]
-    reviews = x[1]
-    total_reviews = 0
-    total_neg_reviews = 0
-    total_points = 0
+    cuisine, reviews = x[0], x[1]
+    total_reviews, total_neg_reviews, total_points = 0, 0, 0
 
     for r in reviews:
-        total_reviews += 1
         review_tuple = r[1]
-        evaluation = review_tuple[1]
-        points = review_tuple[0]
+        evaluation, points = review_tuple[1], review_tuple[0]
 
         if evaluation == "Negative":
             total_neg_reviews += 1
@@ -63,33 +54,24 @@ def aggregate_info(x):
         else:
             total_points += points
 
+        total_reviews += 1
+
     return cuisine, (total_reviews, total_neg_reviews, total_points)
 
 
 # ------------------------------------------
-# FUNCTION remove_info
+# FUNCTION remove_info(x, avgReviews, percentage_f)
 # ------------------------------------------
 def remove_info(x, avgReviews, percentage_f):
-    total_reviews = x[1][0]
-    total_neg_reviews = x[1][1]
+    total_reviews, total_neg_reviews = x[1][0], x[1][1]
     neg_reviews_percentage = get_avg(total_neg_reviews, total_reviews) * 100
 
     if total_reviews >= avgReviews and neg_reviews_percentage < percentage_f:
         return True
-    else:
-        return False
 
 
-# ------------------------------------------
-# FUNCTION aggregate_info
-# Aggregate info per type of cuisine. Now incl avg points
-# Example: (u'Hamburgers', (1676, 107, 11190, 6.676610978520286))
-# ------------------------------------------
 def aggregate_avg_points(x):
-    cuisine = x[0]
-    total_reviews = x[1][0]
-    total_neg_reviews = x[1][1]
-    points = x[1][2]
+    cuisine, total_reviews, total_neg_reviews, points = x[0], x[1][0], x[1][1], x[1][2]
     avg_points_per_review = get_avg(points, total_reviews)
 
     return cuisine, (total_reviews, total_neg_reviews, points, avg_points_per_review)
@@ -107,20 +89,23 @@ def my_main(dataset_dir, result_dir, percentage_f):
     groupByKeyRDD = processItemsRDD.groupBy(lambda x: x[0])
 
     aggRDD = groupByKeyRDD.map(lambda x: aggregate_info(x))
+    # print_rdd(aggRDD)
 
     # Point 2 ------------------------------------------
     #
     avgReviews = get_avg(mapRDD.count(), aggRDD.count())
+    # print(avgReviews)
 
     # Point 3 ------------------------------------------
     #
     removedEntriesRDD = aggRDD.filter(lambda x: remove_info(x, avgReviews, percentage_f))
+    # print_rdd(removedEntriesRDD)
 
     # Point 4 ------------------------------------------
     #
     aggAvgPointsRDD = removedEntriesRDD.map(lambda x: aggregate_avg_points(x))
     solutionRDD = aggAvgPointsRDD.sortBy(lambda x: x[1][3], ascending=False)
-    # print_rdd(solutionRDD)
+    print_rdd(solutionRDD)
 
     solutionRDD.saveAsTextFile(result_dir)
 
